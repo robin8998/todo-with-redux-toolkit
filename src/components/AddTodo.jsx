70f7878b0cfc519f654472a5da683151ec281452
Robin Singh <ranarobin8998@gmail.com>
2025-03-19 11:08:59 +0530

    fix bugs and improove ui

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,29 +1,39 @@
-import React, {  useState } from 'react'
-import {useDispatch} from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { addTodo } from '../features/todo/todoSlice'
 
-
 function AddTodo() {
-    const [input,setInput] = useState('')
-    const dispatch = useDispatch()
-
-    const addTodoHandler=(e)=>{
-        e.preventDefault()
-        dispatch(addTodo(input))
-        setInput('')
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+  
+  const addTodoHandler = (e) => {
+    e.preventDefault()
+    if (input.trim()) {
+      dispatch(addTodo(input))
+      setInput('')
     }
+  }
+
   return (
-    <form onSubmit={addTodoHandler}
-    className='m-5 bg-gray-500'
-     >
-        <input type="text" 
-        placeholder='Enter Todo'
-        value={input}
-        onChange={(e)=> setInput(e.target.value)}
+    <form 
+      onSubmit={addTodoHandler}
+      className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md"
+    >
+      <div className="flex flex-col sm:flex-row gap-4">
+        <input 
+          type="text"
+          placeholder="Enter Todo"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="flex-grow px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
         />
-        <button 
-        className='bg-green-700 w-14 h-8 rounded-xl ml-4 hover:bg-yellow-500'
-        type='submit'>Add</button>
+        <button
+          type="submit"
+          className="px-6 py-2 bg-blue-600 text-white font-medium rounded-lg transition-colors duration-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+        >
+          Add
+        </button>
+      </div>
     </form>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
-import { useSelector ,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { removeTodo } from '../features/todo/todoSlice'
 
 function Todo() {
-    const todos = useSelector((state)=> state.todos)
-    const dispatch = useDispatch()
+  const todos = useSelector((state) => state.todos)
+  const dispatch = useDispatch()
+
   return (
-    <>  <div className='bg-blue-300 p-2 '>
-        {todos.map((todo)=> (
-         <li key={todo.id} 
-         className='list-none '
-         >
-             {todo.text}
-             <button 
-             className='bg-red-700 w-14 h-8 rounded-xl ml-4 hover:bg-orange-700'
-             onClick={()=> dispatch(removeTodo(todo.id))}
-             >X</button>
-         </li>
-        ))}
-        </div>
-    </>
+    <div className="max-w-md mx-auto mt-8 bg-gradient-to-r from-blue-50 to-blue-100 rounded-lg shadow-lg overflow-hidden">
+      <h2 className="bg-blue-600 text-white text-xl font-semibold py-3 px-4">My Todos</h2>
+      
+      {todos.length === 0 ? (
+        <p className="p-6 text-gray-500 text-center">No todos yet. Add one above!</p>
+      ) : (
+        <ul className="divide-y divide-blue-200">
+          {todos.map((todo) => (
+            <li 
+              key={todo.id}
+              className="flex items-center justify-between px-4 py-3 hover:bg-blue-50 transition-colors duration-150"
+            >
+              <span className="text-gray-800 break-words flex-grow mr-4">{todo.text}</span>
+              <button
+                onClick={() => dispatch(removeTodo(todo.id))}
+                className="bg-red-500 text-white w-8 h-8 rounded-full flex items-center justify-center transition-colors duration-300 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                aria-label="Remove todo"
+              >
+                ×
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
